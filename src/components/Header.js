fa16ff339bd6ed1fc94401677a241461761c2430
Brandonbay <undftdcreed@gmail.com>
2023-05-30 13:54:02 -0700

    2nd commit for the day

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,31 @@
 import { Link } from "react-router-dom";
-
-
+import {FaBars, FaTimes } from "react-icons/fa";
+import { useRef } from "react";
 
 
 function Header(props) {
-  //inline style for the nav tag
-  const navStyle = {
-    display: "flex",
-    justifyContent: "space-around",
-    border: "3px solid black",
-    padding: "8px",
-    width: "90%",
-    margin: "auto",
-  };
-
+    
+    
+    //inline style for the nav tag
+    const navStyle = {
+        display: "flex",
+        justifyContent: "space-around",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: "auto",
+    };
+    
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
 
     return (
         <header className="interactive-header">
             <h1>Magic Life on Deck </h1>
-            <nav style={navStyle}>
+            <nav ref={navRef} style={navStyle}>
+                
                 <Link to={"/"}>
                     <div>Home</div>
                     </Link>
@@ -28,7 +35,13 @@ function Header(props) {
                 <Link to={"/create"}>
                     <div>Create a Deck</div>
                 </Link>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
             </nav>
+                <button className="nav-btn" onClick={showNavbar}>
+                <FaBars />
+                </button>
         </header>
 
   );
